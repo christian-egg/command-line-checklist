06e2ff43737bcc5e38749428d560354046204ac7
christian-egg <christian@cclassen.com>
2021-06-18 16:58:22 -0500

    Created the create_confirmation_question method

--- checklist.py
+++ checklist.py
@@ -9,12 +9,28 @@ LIST_FILE_NAME = "list.txt"
 def print_banner(text):
     print(Figlet(font='doom').renderText(text))
 
+def get_file_text(file_name)
+    return cat(file_name)
+
 def get_list_items(list_text):
     if (len(list_text) == 0):
         return None
     
     list = list_text.strip()
     items = list.split("\n")
+    
+    return items
+
+def create_menu_question(items):
+    return questionary.rawselect(
+        "Choose to edit an existing item or create a new one",
+        choices=[{
+            'name': item.strip()} for item in items, 
+            "Add a new item",
+            "Clear list",
+            "Quit"
+        ]
+    )
 
 class Checklist(cli.Application):
     VERSION = "1.0"
@@ -23,5 +39,6 @@ class Checklist(cli.Application):
     def main(self):
         print_banner("CLI Checklist")
         items = get_list_items(list_text)
+        if (items == None):
 
 
